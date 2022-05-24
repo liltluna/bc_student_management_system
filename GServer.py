af6936c3d38a451aab03a7d7781b21beacf543ca
BreathAir <88447898+BreathAir@users.noreply.github.com>
2022-05-24 14:16:15 +0800

    Update GServer.py

--- GServer.py
+++ GServer.py
@@ -4,7 +4,6 @@ import threading
 import pymysql
 
 import GPacket
-import GUser
 
 
 class Server:
@@ -44,6 +43,7 @@ class Server:
 
         server_socket.bind(ADDR)
         server_socket.listen()
+
         while True:
             client_socket, addr = server_socket.accept()
             response_thread = ResponseClient()
@@ -84,7 +84,7 @@ class Server:
 class ResponseClient(threading.Thread):
     def __init__(self):
         super().__init__()
-        self.__source_server = None
+        # self.__source_server = None
         self.__conn = None
         self.__target_client = None
         self.__packet = None
@@ -111,7 +111,17 @@ class ResponseClient(threading.Thread):
     def getClient(self):
         return self.__target_client
 
+    def recordOperate(self, user_operator, user_target, operate_type):
+        SQL_rec = """insert into operate_record values ('""" + user_operator \
+                  + "'," + "'" + operate_type + "'," \
+                  + "'" + user_target + "'," + 'default);'
+        cur = self.__conn.cursor()
+        cur.execute(SQL_rec)
+        self.__conn.commit()
+        cur.close()
+
     def responseLogin(self):
+        """回应登录请求"""
         user = self.__packet.getOperator()
         cur = self.__conn.cursor()
         SQL = """select password from stu_info where user_name = '""" + user.getUser_name() + "'"
@@ -122,6 +132,114 @@ class ResponseClient(threading.Thread):
             packet_response.setPasswordSignal(True)
             bp_response = pickle.dumps(packet_response)
             self.__target_client.send(bp_response)
+        else:
+            packet_response = GPacket.Packet_response_login()
+            packet_response.setPasswordSignal(False)
+            bp_response = pickle.dumps(packet_response)
+            self.__target_client.send(bp_response)
+        self.recordOperate(user.getUser_name(), user.getUser_name(), 'l')
+        cur.close()
+
+    def responseDelete(self):
+        """回应删除请求"""
+        user = self.__packet.getOperator()
+        info = self.__packet.getDelete_info()
+        cur = self.__conn.cursor()
+        SQL_exe = """delete from stu_info where student_number = '""" + info.getStudent_number() + "'"
+        cur.execute(SQL_exe)
+        self.__conn.commit()
+        # 加上操作是否完成的判断工作，加上返回给客户端的数据,目前只会返回成功。
+        cur.close()
+        packet_response = GPacket.Packet_response_is_successful()
+        packet_response.setOperate_result(True)
+        bp_response = pickle.dumps(packet_response)
+        self.__target_client.send(bp_response)
+        self.recordOperate(user.getUser_name(), info.getUser_name(), 'd')
+
+    def responseUpdate(self):
+        user = self.__packet.getOperator()
+        info = self.__packet.getUpdate_info()
+        cur = self.__conn.cursor()
+        info_list = [('name', str(info.getName())),
+                     ('sex', str(info.getSex())),
+                     ('student_number', str(info.getStudent_number())),
+                     ('group_number', str(info.getGroup_number())),
+                     ('qq_number', str(info.getQQ_number())),
+                     ('user_type', str(info.getUser_type())),
+                     ('password', str(info.getPassword())),
+                     ('user_name', str(info.getUser_name())), ]
+        SQL_exe = """update stu_info set """
+        for column, data in info_list:
+            SQL_exe += (column + " = '" + data + "', ")
+        SQL_exe = SQL_exe[:-1] + ';'
+        cur.execute(SQL_exe)
+        self.__conn.commit()
+        cur.close()
+        self.recordOperate(user.getUser_name(), info.getUser_name(), 'u')
+        # 告知客户端是否操作成功
+        packet_response = GPacket.Packet_response_is_successful()
+        packet_response.setOperate_result(True)
+        bp_response = pickle.dumps(packet_response)
+        self.__target_client.send(bp_response)
+
+    def responseAdd(self):
+        user = self.__packet.getOperator()
+        info = self.__packet.getAdd_info()
+        cur = self.__conn.cursor()
+        info_list = [str(info.getUser_name()),
+                     str(info.getSex()),
+                     str(info.getStudent_number()),
+                     str(info.getGroup_number()),
+                     str(info.getQQ_number()),
+                     str(info.getUser_type()),
+                     str(info.getPassword()),
+                     str(info.getUser_name()),
+                     ]
+        SQL_exe = """insert into stu_info values ('"""
+        for data in info_list:
+            SQL_exe += (data + "' , '")
+        SQL_exe = SQL_exe[:-3] + ');'
+        cur.execute(SQL_exe)
+        self.__conn.commit()
+        cur.close()
+        packet_response = GPacket.Packet_response_is_successful()
+        packet_response.setOperate_result(True)
+        bp_response = pickle.dumps(packet_response)
+        self.__target_client.send(bp_response)
+        self.recordOperate(user.getUser_name(), info.getUser_name(), 'a')
+
+    def responseSearch(self):
+        user = self.__packet.getOperator()
+        cur = self.__conn.cursor()
+        packet_response = GPacket.Packet_response_is_successful()
+        SQL_exe = """select * from stu_info"""
+
+        if user.getUser_type() == 0:
+            SQL_exe += ';'
+            cur.execute(SQL_exe)
+            data = cur.fetchall()
+        elif user.getUser_type() == 1:
+            group_number = user.getGroup_number()
+            SQL_exe += "where group_number = '" + group_number + "';"
+            cur.execute(SQL_exe)
+        elif user.getUser_type() == 2:
+            student_number = user.getStudent_number()
+            SQL_exe += "where student_number = '" + student_number + "';"
+            cur.execute(SQL_exe)
+        else:
+            packet_response.setOperate_result(False)
+            bp_response = pickle.dumps(packet_response)
+            self.__target_client.send(bp_response)
+            exit(0)
+        data_packet = GPacket.Packet_response_data()
+        data_packet.setData(data)
+        bd = pickle.dumps(data)
+        self.__target_client.send(bd)
+        cur.close()
+        packet_response.setOperate_result(False)
+        bp_response = pickle.dumps(packet_response)
+        self.__target_client.send(bp_response)
+        self.recordOperate(user.getUser_name(), user.getUser_name(), 's')
 
     def run(self):
         while True:
@@ -136,7 +254,8 @@ class ResponseClient(threading.Thread):
                 pass
         print('end...')
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     db = {
         'host': 'localhost',
         'user': 'root',
