de43ec0b37df1add7f20e545824df559fdf3e84b
BreathAir <88447898+BreathAir@users.noreply.github.com>
2022-06-01 22:20:47 +0800

    Update GClinet.py

--- GClinet.py
+++ GClinet.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 from socket import *
+
+import GPacket
 from HandleProcess import *
 from GPacket import *
 from GUser import *
@@ -42,8 +44,8 @@ class client:
         try:
             self.tcpCliSock.connect(self.ADDR)
         except:
-             messagebox.askokcancel(title='提示', message='连接失败，请检查你的网络')
-             exit(0)
+            messagebox.askokcancel(title='提示', message='连接失败，请检查你的网络')
+            exit(0)
         else:
             self.Login()
 
@@ -129,26 +131,28 @@ class client:
 
         """判断权限，根据权限执行不同的内容，显示不同的页面
         """
+
         self.set_user_name(name_g)
         self.set_password(ps_g)
 
         GM = GUser.GroupMember()
-        GM.setUser_name(self.name)
-        GM.setPassword(self.ps)
+        GM.setUser_name('bamboo')
+        GM.setPassword('123456')
         GM.setUser_type(self.authority)
+        operator = GM
 
-        operator = Packet_operate()
-        operator.setOperator(GM)
         packet1 = Packet_login()  # 登陆请求
+        packet1.setOperator(GM)
         packet2 = Packet_search_info()  # 查询请求
-        packet2.setOperator(operator)
+        packet2.setOperator(GM)
         res1 = self.communicate(packet1)
         res2 = self.communicate(packet2)
 
-        if not res1 or not res2:
+        if not res1:
+            print(res1)
             messagebox.askokcancel(title='提示', message='找不到对象')
             exit(0)
-        
+
         self.fm1.pack_forget()
         self.fm2.pack_forget()
         self.fm3.pack()
@@ -179,28 +183,28 @@ class client:
             '''普通组员'''
             ssbj = Message(self.fm3, text='所属班级')
             ssxz = Message(self.fm3, text='所属小组')
-            ssbj_M=Message(self.fm3,text='2020211317')
-            ssxz_M=Message(self.fm3,text=res2[0][3])
+            ssbj_M = Message(self.fm3, text='2020211317')
+            ssxz_M = Message(self.fm3, text=res2[0][3])
 
             ssbj.place(x=500, y=400)
             ssxz.place(x=500, y=500)
-            ssbj_M.place(x=700,y=400)
-            ssxz_M.place(x=700,y=500)
+            ssbj_M.place(x=700, y=400)
+            ssxz_M.place(x=700, y=500)
         if self.authority == 2:
             '''组长'''
             ssbj = Message(self.fm3, text='所属班级')
             glxz = Message(self.fm3, text='管理小组')
-            glxz_M=Message(self.fm3,text=res2[0][3])
-            ssbj_M=Message(self.fm3,text='2020211317')
+            glxz_M = Message(self.fm3, text=res2[0][3])
+            ssbj_M = Message(self.fm3, text='2020211317')
             ssbj.place(x=500, y=400)
             glxz.place(x=500, y=500)
-            ssbj_M.place(x=700,y=400)
+            ssbj_M.place(x=700, y=400)
         if self.authority >= 3:
             '''授课老师'''
             glbj = Message(self.fm3, text='管理班级')
-            glbj_M=Message(self.fm3,text='2020211317')
+            glbj_M = Message(self.fm3, text='2020211317')
             glbj.place(x=500, y=400)
-            glbj_M.place(x=700,y=400)
+            glbj_M.place(x=700, y=400)
 
         tree = ttk.Treeview(M_N.sub_frame, columns=(
             'name', 'sex', 'student_number', 'group_number', 'qq_number', 'user_type', 'password', 'username'),
@@ -229,10 +233,10 @@ class client:
             iid = tree.selection()
             packet = Packet_delete_info()
             packet.setOperator(operator)
-            
+
             d = tree.item(tree.focus())
             delee = GUser.GroupMember()
-            delee.setStuent_number(d['student_number'])
+            delee.setStudent_number(d['student_number'])
             packet.setDelete_info(delee)
             self.communicate(packet)
 
@@ -243,8 +247,8 @@ class client:
             add = Tk()
             name_M = Message(add, text='姓名')
             name = Entry(add)
-            sex_M =Message(add, text='性别')
-            sex = ttk.Combobox(add,  values=['M','F'], state='readonly')
+            sex_M = Message(add, text='性别')
+            sex = ttk.Combobox(add, values=['M', 'F'], state='readonly')
             name_M.grid(row=0, column=0)
             name.grid(row=0, column=1)
             sex_M.grid(row=1, column=0)
@@ -260,7 +264,7 @@ class client:
             gro.grid(row=3, column=1)
 
             ut_M = Message(add, text='用户等级')
-            ut  = ttk.Combobox(add, values=['一般用户','组长','教师'], state='readonly')
+            ut = ttk.Combobox(add, values=['一般用户', '组长', '教师'], state='readonly')
             ps_M = Message(add, text='密码')
             ps = Entry(add)
             ut_M.grid(row=4, column=0)
@@ -280,12 +284,15 @@ class client:
             addB = Button(add, text='确定', command=lambda: adding(
                 {'name': name.get(), 'sex': sex.get(), 'student_number': stu.get(), 'group_number': gro.get(),
                  'qq_number': qq.get(), 'user_type': ut.get(), 'password': ps.get(), 'username': user.get()}))
-            addB.grid(row=8,column=1)
-
-            
+            addB.grid(row=8, column=1)
 
         def adding(get):
             """向服务器发送增加请求，并在收到回复后，进行增加"""
+            if get['user_type']=='一般用户':
+                    get['user_type']=1
+            elif get['user_type']=='组长':
+                    get['user_type']=2
+            else :get['user_type']=3
             packet = Packet_add_info()
             packet.setOperator(operator)
             addMember = GUser.GroupMember()
@@ -303,16 +310,9 @@ class client:
             packet.setAdd_information(addMember)
             if self.communicate(packet):
                 con = []
-                if get['user_type']=='一般用户':
-                    get['user_type']=1
-                elif get['user_type']=='组长':
-                    get['user_type']=2
-                else :get['user_type']=3
-
-                
                 for key in get:
                     con.append(get[key])
-                tree.insert('', END, values=get)
+            tree.insert('', END, values=con)
 
         for itm in res2:
             tree.insert("", END, values=itm)
@@ -360,22 +360,20 @@ class client:
            ip与端口在创建类时进行指定，
            st表示要传输的内容"""
 
-        
-        u = GUser.Master()
-        u.setUser_name('bamboo')
-        u.setPassword('12345**6')
-        packet.setOperator(u)
         bp = pickle.dumps(packet)
         data1 = bp
 
-    
         self.tcpCliSock.send(data1)
         data = self.tcpCliSock.recv(1024)
         res = pickle.loads(data)
-        result = res.getPasswordSignal()
-        
+        result = None
+        if type(res) == type(GPacket.Packet_response_login()):
+            result = res.getPasswordSignal()
+        elif type(res) == type(GPacket.Packet_response_is_successful()):
+            result = res.getOperate_result()
+        elif type(res) == type(GPacket.Packet_response_data()):
+            result = res.getData()
 
-        self.tcpCliSock.close()
         return result
 
 
